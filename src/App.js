ea9c333272f00638eaf8bf0773e78dab0434675d
Nurşah Demirpolat <nursah.demirpolat@vodafone.com>
2023-01-12 22:54:57 +0300

    useReducer()

--- src/App.js
+++ src/App.js
@@ -1,96 +1,50 @@
-import { createElement,Fragment, useEffect, useState } from 'react';
-// import Tailwind from "./Tailwind";
-// import './tailwind.css'
-// import Button from './Components/Button.js'
-// import Tab from './Components/Tab.js'
-import Test from './Test.js'
+import { useState ,useReducer} from 'react';
+import todoReducer from "./reducers/todoReducer";
 
-// function Profile(){
-//   return(
-//     <div>
-//       Burası profile tabı
-//     </div>
-//   )
-// }
 
 function App() {
 
-  const [show,setShow] = useState(false)
+  const [state,dispatch] = useReducer(todoReducer,{
+    todos:[],
+    todo:''
+  });
 
-  return (
+// const [todos,setTodos] = useState([])
+// const [todo,setTodo] = useState()
+
+const submitHandle = e => {
+  e.preventDefault()
+  dispatch({
+    type:'ADD_TODO',
+    todo:state.todo
+  })
+  // setTodos([...todos,todo])
+  // setTodo('')
+}
+
+const onChange = e => {
+  // setTodo(e.target.value)
+  dispatch({
+    type:'SET_TODO',
+    value:e.target.value
+  })
+}
+
+  return(
     <>
-      <button onClick={()=> setShow(show => !show)}>
-        {show ? 'Gizle' : 'Göster'}
-      </button>
-      {show && <Test/>}
+      <h1>UseReducer</h1>
+      <form onSubmit={submitHandle}>
+        <input type="text" value={state.todo} onChange={onChange}/>
+        <button type="submit">Ekle</button>
+      </form>
+      <ul>
+        {state.todos.map((todo,index) => (
+          <li key={index}>
+            {todo}
+          </li>
+        ))}
+      </ul>
     </>
-  );
-
-  // const name = "nursah"
-  // const todos=['todo1','tofo2','todo3']
-
-  // const [activeTab,setActiveTab] = useState(1)
-
-  // function Button(props){
-  //   return <button>{props.text}</button>
-  // }
-
-  // const h1=createElement('h1',null,'nursah.com')
-  // const ul=createElement('ul',null,todos.map(todo => createElement('li',null,todo)))
-  // const button=createElement(Button,{
-  //   text:'button texti'
-  // },null)
-
-  // return createElement('main',{
-  //   className:'test',
-  //   id:'main',
-  // },h1,ul,button)
-
-
-
-//   return (
-//    <>
-
-//   <div style={{padding:20}}>
-//     <button onClick={() => setActiveTab(2)}>
-//       Aktif tabı değiştir
-//     </button>
-//     <Tab activeTab={activeTab} onChange={tabIndex => setActiveTab(tabIndex)}>
-//       <Tab.Panel title="Profil"><Profile/></Tab.Panel>
-//       <Tab.Panel title="Hakkında">2.Tab</Tab.Panel>
-//       <Tab.Panel title="Ayarlar">3.Tab</Tab.Panel>
-//     </Tab>
-//     {activeTab === 2 && (
-//         <div>
-//           Burası ekstra bi alan
-//         </div>
-//     )}
-//   </div>
-
-//    <div style={{padding:20}}>
-//     <Button>
-//       Buton Örneği
-//     </Button>
-//     <Button variant="success">
-//       Buton Örneği
-//     </Button>
-//       <Button text="Buton Örneği" variant="danger"/>
-//       <Button text="Buton Örneği" variant="warning"/>
-//    </div>
-//     <h1 tabIndex="3" style={{color:'blue', backgroundColor:'yellow'}}>nursah.com</h1>
-//     <label htmlFor="search" tabIndex="2" onClick={() => alert('merhaba')} >Arama</label>
-//     <input type="text" id="search" tabIndex="1"/>
-//     <ul>
-//       {name + " gunes"}
-//       {todos.map((todo,index)=> (
-//         <li key={index}>
-//           {todo}
-//         </li>
-//       ))}
-//     </ul>
-//    </>
-//   );
-// }
-
+  )
 }
 export default App;
